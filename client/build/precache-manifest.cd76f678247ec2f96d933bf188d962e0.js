4cf65542019aadbfebbb0c5005094db3a6a9c2d7
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2020-09-19 11:36:55 -0700

    adding more mobile code

---
+++ client/build/precache-manifest.cd76f678247ec2f96d933bf188d962e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a24b099f62e2edba3e25add2aa7ebe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f919755f63e38971f92",
+    "url": "/static/css/main.e465adba.chunk.css"
+  },
+  {
+    "revision": "66247e84b03187073d03",
+    "url": "/static/js/2.75a1cc56.chunk.js"
+  },
+  {
+    "revision": "7b1affef45e2b57a268c1974299a48a9",
+    "url": "/static/js/2.75a1cc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f919755f63e38971f92",
+    "url": "/static/js/main.eda1549b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3ef4d8894a068a6bd1f7549004cdefe6",
+    "url": "/static/media/bg3.3ef4d889.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
--- server.js
+++ server.js
@@ -267,4 +267,15 @@ console.log(email)
       } 
   })
 
-app.listen(5000,() => console.log("app running on port 5000"))
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+  const PORT = process.env.PORT || 5000
+
+app.listen(PORT,() => console.log("app running on port 5000"))
